aec1879abdec6600389be21af79f55a3ff9a17f5
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-27 17:55:51 +0100

    aufgabe 3

--- src/Aufgabe3/Aufgabe2.java
+++ src/Aufgabe3/Aufgabe2.java
@@ -1,5 +1,7 @@
 package Aufgabe3;
 
+import javax.swing.*;
+
 public class Aufgabe2
 {
     public static void main(String[] args)
@@ -7,13 +9,36 @@ public class Aufgabe2
         for(int i = 0; i<3; i+=2)//wird jedes mal um 2 erhöht
         {
             printName();
+
+        }
+        //der Name wird nur 2 mal ausgegeben und zwar bei durchlauf 0 und 2 da der nächste durchlauf 4 wäre und dieser aber größer als 4 ist
+
+
+        //aufgabe3
+        for(int j = 0; j<5; j++)
+        {
+            printName();//NAme wird ausgegeben
+            printAge(19 + j);//Alter wird ausgegeben und bei jedem durchlauf um 1 erhöht
+
         }
 
-        //der Name wird nur 2 mal ausgegeben und zwar bei durchlauf 0 und 2 da der nächste durchlauf 4 wäre und dieser aber größer als 4 ist
+
     }
 
-    public static void printName()//methode zum ausführen des Namens
+
+
+    public static void printName()//methode zum ausführen des Namens es wird nichts zurückgegeben
     {
         System.out.println("Larissa Blachfelder");
+    }
+
+
+    //aufgabe3
+
+    public static int printAge(int age)//es wird ein int zurückgegeben
+    {
+
+        System.out.println("Alter: " + age);
+        return age; //hier wird das ALter als int zurückgegeben
     }
 }
